db26f53844debdb697a514ea62541bd1cad6c6df
Olga Bobrova <obobrova@23andme.com>
2016-08-28 18:21:18 -0700

    fixup

--- setup.py
+++ setup.py
@@ -2,10 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='stegolib',
-    version='0.1dev',
-    packages=['stegolib',],
+    version='0.1.dev0',
+    packages=['stegolib'],
     license='The Unlicense',
     long_description=open('README.md').read(),
-    setup_requires=['pytest-runner',],
-    tests_require=['pytest','pytest-pep8'],
+    setup_requires=['pytest-runner'],
+    tests_require=[
+        'pytest',
+        'pytest-pep8'
+    ],
 )
